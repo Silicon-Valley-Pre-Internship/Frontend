9f6bba3733d58f04164e3542d84260f991f4300a
fairyroad <kuj9628@naver.com>
2021-07-13 17:32:24 +0900

    Add : Bottom Tab Bar and modify design

--- login-test/src/theme.js
+++ login-test/src/theme.js
@@ -1,10 +1,12 @@
 const Colors = {
     white: '#ffffff',
     black: '#111111',
+    pink: '#EF9DA9',
     main: '#3679fe'
 };
 
 export const theme = {
     background: Colors.white,
+    background2: Colors.pink,
     text: Colors.text
 };
