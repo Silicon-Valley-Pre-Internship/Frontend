ba6d72f89452b46fda56327cac29d31f84e7ae45
118dg <54856465+118dg@users.noreply.github.com>
2021-07-13 16:49:03 +0900

    input component 작업중
    930

--- login-test/src/screens/Signin.js
+++ login-test/src/screens/Signin.js
@@ -1,7 +1,7 @@
-import React, {useContext} from 'react';
+import React, { useContext, useState } from 'react';
 import { ThemeContext } from 'styled-components/native';
 import styled from 'styled-components';
-import { Button, Image } from '../compoments';
+import { Button, Image, Input } from '../compoments';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Container = styled.View`
@@ -14,21 +14,21 @@ const Container = styled.View`
   padding-bottom: ${({insets: {bottom}}) => bottom}px;
 `;
 
-const StyledText = styled.Text`
-    font-size: 30px;
-    color: #111111;
-`;
-
 const LOGO='https://firebasestorage.googleapis.com/v0/b/rn-chat-55bad.appspot.com/o/linist_logo.png?alt=media';
 
 const Signin = ({navigation}) => {
     const insets = useSafeAreaInsets();
     const theme = useContext(ThemeContext);
 
+    //useState를 이용해서 email, password 상태 변수를 만든다
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <Container insets={insets}>
             <Image url={LOGO} />
-            <StyledText>Signin</StyledText>
+            <Input label="Email" placeholder="Email" returnKeyType="next" value={email} onChangeText={setEmail} />
+            <Input label="Password" placeholder="Password" returnKeyType="done" value={password} onChangeText={setPassword} />
             <Button title="sign in" onPress={() => console.log('signin')} />
             <Button 
                 title="or sign up"
@@ -36,8 +36,8 @@ const Signin = ({navigation}) => {
                 containerStyle={{ marginTop: 0, backgroundColor: 'transparent' }}
                 textStyle={{color: theme.btnTextLink, fontSize: 18}}
             />
-            <Button title="MainScreen" onPress={() => navigation.navigate('MainScreen')} />
-            <Button title="Background" onPress={() => navigation.navigate('Background')} />
+            {/* <Button title="MainScreen" onPress={() => navigation.navigate('MainScreen')} /> */}
+            {/* <Button title="Background" onPress={() => navigation.navigate('Background')} /> */}
         </Container>
     );
 };
--- login-test/src/theme.js
+++ login-test/src/theme.js
@@ -2,12 +2,13 @@ const Colors = {
     white: '#ffffff',
     black: '#111111',
     main: '#3679fe',
-    grey: '#d5d5d5'
+    grey_0: '#d5d5d5',
+    grey_1: '#a6a6a6',
 };
 
 export const theme = {
     background: Colors.white,
-    text: Colors.text,
+    text: Colors.black,
 
     //Button
     btnBackground: Colors.main,
@@ -16,4 +17,10 @@ export const theme = {
 
     //Image
     imgBackground: Colors.white,
+
+    //Input
+    inputBackground: Colors.white,
+    inputLabel: Colors.grey_1,
+    inputPlaceholder: Colors.grey_1,
+    inputBorder: Colors.grey_1,
 };
