d4657fe358f52fe94ebc387dd5937d8410ca2ca0
118dg <54856465+118dg@users.noreply.github.com>
2021-07-29 16:27:35 +0900

    code

--- login-test/src/components/CardComponent.js
+++ login-test/src/components/CardComponent.js
@@ -1,27 +1,63 @@
-import React from 'react';
-
+import React, { Component } from "react";
 import {
     View,
     Text,
-    StyleSheet
-} from 'react-native';
-import { Profile } from '../screens';
+    StyleSheet,
+    Image
+} from "react-native";
 
-class CardComponent extends Component{
-    render(){
+class CardComponent extends Component {
+
+    render() {
+
+        const images = {
+
+            "1": require('../assets/feed_images/1.jpg'),
+            "2": require('../assets/feed_images/2.jpg'),
+            "3": require('../assets/feed_images/3.png')
+        }
+
         return (
             <Card>
                 <CardItem>
                     <Left>
-                        <Thumbnail source={require('../assets/me.jpg')}/>
+                        <Thumbnail source={require('../assets/me.png')} />
                         <Body>
-                            <Text>UserName</Text>
-                            <Text note>July 29, 2021</Text>
+                            <Text>Varun </Text>
+                            <Text note>Jan 15, 2018</Text>
                         </Body>
                     </Left>
                 </CardItem>
+                <CardItem cardBody>
+                    <Image source={images[this.props.imageSource]} style={{ height: 200, width: null, flex: 1 }} />
+                </CardItem>
+                <CardItem style={{ height: 45 }}>
+                    <Left>
+                        <Button transparent>
+                            <Icon name="ios-heart-outline" style={{ color: 'black' }} />
+                        </Button>
+                        <Button transparent>
+                            <Icon name="ios-chatbubbles-outline" style={{ color: 'black' }} />
+                        </Button>
+                        <Button transparent>
+                            <Icon name="ios-send-outline" style={{ color: 'black' }} />
+                        </Button>
+
+
+                    </Left>
+                </CardItem>
+
+                <CardItem style={{ height: 20 }}>
+                    <Text>{this.props.likes} </Text>
+                </CardItem>
                 <CardItem>
-                    <Image soure={require('../assets/profile.png')} style={{height: 200, width: null, flex: 1}} />
+                    <Body>
+                        <Text>
+                            <Text style={{ fontWeight: "900" }}>varun
+                            </Text>
+                            Ea do Lorem occaecat laborum do. Minim ullamco ipsum minim eiusmod dolore cupidatat magna exercitation amet proident qui. Est do irure magna dolor adipisicing do quis labore excepteur. Commodo veniam dolore cupidatat nulla consectetur do nostrud ea cupidatat ullamco labore. Consequat ullamco nulla ullamco minim.
+                        </Text>
+                    </Body>
                 </CardItem>
             </Card>
         );
@@ -29,7 +65,7 @@ class CardComponent extends Component{
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
--- login-test/src/screens/Instagram.js
+++ login-test/src/screens/Instagram.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet
+} from "react-native";
 import styled from 'styled-components/native';
-import {Button} from '../components';
+
+import CardComponent from '../components/CardComponent'
 
 const Container = styled.View`
     flex: 1;
     background-color: ${({theme}) => theme.background};
 `;
 
-const Instagram = () => {
+class Instagram extends Component {
 
-    return (
-        <Container>
-            <Button
-                title="instagram"
-            />
-        </Container>
-    );
-};
+    static navigationOptions = {
+
+        tabBarIcon: ({ tintColor }) => (
+            <Icon name="ios-home" style={{ color: tintColor }} />
+        )
+    }
+
+    render() {
+        return (
+            <Container style={styles.container}>
+                    <CardComponent imageSource="1" likes="101" />
+                    <CardComponent imageSource="2" likes="201" />
+                    <CardComponent imageSource="3" likes="301" />
+            </Container>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    }
+});
 
 export default Instagram;
